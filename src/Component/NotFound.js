57771e1b6fecdb2931204e9f58a1d67cab81b71e
Rkely <tellyissa88@gmail.com>
2020-03-04 18:58:15 +0100

    Premier commit

---
+++ src/Component/NotFound.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class NotFound extends Component {
+    render() {
+        return (
+            <div>
+                Y'a rien ici
+            </div>
+        );
+    }
+}
+
+export default NotFound;
